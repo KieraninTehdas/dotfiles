c327d3ee13fd0c9f176e23b97b961cbbce3d1f21
Kieran Porter <kieranintehdas@gmail.com>
2020-06-27 18:03:32 +0100

    Add aliases file for omz

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,6 @@
+alias zshconfig="vim ~/.zshrc"
+alias zshsource="source ~/.zshrc"
+alias ls="lsd"
+alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
