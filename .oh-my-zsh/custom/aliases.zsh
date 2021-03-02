f09ab95896520b9222a44fd106cbc6a05a192ff4
Kieran Porter <kieranintehdas@gmail.com>
2021-03-02 12:20:23 +0000

    Add aliases for postman, markdown rendering, and zsh alias editing

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,9 @@
 alias zshconfig="vim ~/.zshrc"
 alias zshsource="source ~/.zshrc"
+alias zshalias="vim ~/.oh-my-zsh/custom/aliases.zsh"
 alias ls="lsd"
 alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+alias mdr='mdr_linux_amd64'
+alias postman='nohup /opt/Postman/app/Postman & disown'
